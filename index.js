8a767574216f7177d3bdb80daff58a9389688a52
黄俊铭 <huangjunming@huangjunmingdeMacBook-Pro.local>
2019-11-27 00:41:10 +0800

    feat: normal node server

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+  res.writeHead(200)
+  res.end = 'hello nodejs'
+})
+
+server.listen(3000, () => {
+  console.log('server started at port 3000')
+})
