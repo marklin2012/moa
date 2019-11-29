f3b2cb0a4037a3653168cf473f0b8de4160693fa
林艺 <linyi@ECCMAC-C02WF70G.local>
2019-11-29 17:28:42 +0800

    feat: async function compose

--- compose_test.js
+++ compose_test.js
@@ -1,27 +1,47 @@
-function fn1() {
+async function fn1(next) {
   console.log('fn1')
+  next && await next()
   console.log('fn1 end')
 }
 
-function fn2() {
+async function fn2(next) {
   console.log('fn2')
+  next && await next()
   console.log('fn2 end')
 }
 
-function fn3() {
+async function fn3(next) {
   console.log('fn3')
+  next && await next()
   console.log('fn3 end')
 }
 
 // fn3(fn2(fn1()))
 
-const compose = (middlewares) => () => {
-  [first, ...others] = middlewares
-  let ret = first()
-  others.forEach(fn => {
-    ret = fn(ret)
-  })
-  return ret
+// const compose = (middlewares) => () => {
+//   [first, ...others] = middlewares
+//   let ret = first()
+//   others.forEach(fn => {
+//     ret = fn(ret)
+//   })
+//   return ret
+// }
+
+function compose(middlewares) {
+  return function () {
+    return dispatch(0)
+    function dispatch(i) {
+      let fn = middlewares[i]
+      if (!fn) {
+        return Promise.resolve()
+      }
+      return Promise.resolve(
+        fn(function () {
+          return dispatch(i + 1)
+        })
+      )
+    }
+  }
 }
 
 const middlewares = [fn1, fn2, fn3]
